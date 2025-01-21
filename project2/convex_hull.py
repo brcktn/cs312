0111a3c06903316daf9e8b3537169a04d0dcfea0
brcktn <29804204+brcktn@users.noreply.github.com>
2025-01-20 20:12:33 -0700

    added some more functions, need to finish implementing combine hulls

--- project2/convex_hull.py
+++ project2/convex_hull.py
@@ -5,7 +5,74 @@
 
 def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]:
     """Return the subset of provided points that define the convex hull"""
+    if len(points) < 3:
+        return points
+
+    if len(points) == 3:
+        if is_ccw(points[0], points[1], points[2]):
+            return points
+        else:
+            return [points[2], points[1], points[0]]
+
     return []
+
+
+def split_points(
+    points: list[tuple[float, float]]
+) -> tuple[list[tuple[float, float]], list[tuple[float, float]]]:
+    """Return a tuple of two lists of points split along the y-axis"""
+    points.sort(key=lambda x: x[0])
+    mid = len(points) // 2
+    return points[:mid], points[mid:]
+
+
+def combine_hulls(
+    left: list[tuple[float, float]], right: list[tuple[float, float]]
+) -> list[tuple[float, float]]:
+    """Return the convex hull of the two provided hulls
+    Assumes that both hulls are sorted in ccw order"""
+    rigthmost_left_index = max(enumerate(left), key=lambda x: x[1][0])[
+        0
+    ]  # index of rightmost point in left hull
+    leftmost_right_index = min(enumerate(right), key=lambda x: x[1][0])[
+        0
+    ]  # index of leftmost point in right hull
+
+    i = rigthmost_left_index
+    j = leftmost_right_index
+
+    while True:
+        if is_ccw(left[(i + 1) % len(left)], left[i], right[j]):
+            i = (i + 1) % len(left)
+        elif is_ccw(left[i], right[j], right[(j - 1) % len(right)]):
+            j = (j - 1) % len(right)
+        else:
+            top_chord_indecies = (i, j)
+            break
+
+    i = rigthmost_left_index
+    j = leftmost_right_index
+
+    while True:
+        if is_ccw(right[j], left[i], left[(i - 1) % len(left)]):
+            i = (i - 1) % len(left)
+        elif is_ccw(right[(j + 1) % len(right)], right[j], left[i]):
+            j = (j + 1) % len(right)
+        else:
+            bottom_chord_indecies = (i, j)
+            break
+
+    
+
+
+def keep_between_indecies(points: list[tuple[float, float]], indecies: tuple[int, int]) -> list[tuple[float, float]]:
+    """Return the subset of points that are between the provided indecies"""
+    
+    start, end = indecies
+    if start < end:
+        return points[start:end + 1]
+    else:
+        return points[start:] + points[:end + 1]
 
 
 def is_ccw(
@@ -28,5 +95,3 @@ def is_ccw(
 def cross_product(a: tuple[float, float], b: tuple[float, float]) -> float:
     """Return the cross product of the vectors a and b"""
     return a[0] * b[1] - a[1] * b[0]
-
-
--- project2/tests/test_funcs.py
+++ project2/tests/test_funcs.py
@@ -1,6 +1,12 @@
 import pytest
 
-from project2.convex_hull import is_ccw, cross_product
+from project2.convex_hull import (
+    is_ccw,
+    cross_product,
+    compute_hull,
+    split_points,
+    keep_between_indecies,
+)
 
 
 def test_is_ccw():
@@ -25,3 +31,25 @@ def test_cross_product():
     assert cross_product((3, 0), (0, 2)) == 6
 
 
+def test_convex_hull_2_points():
+    assert compute_hull([(1, 2), (3, 4)]) == [(1, 2), (3, 4)]
+    assert compute_hull([(3, 4), (1, 2)]) == [(3, 4), (1, 2)]
+    assert compute_hull([(1, 2), (1, 2)]) == [(1, 2), (1, 2)]
+
+
+def test_convex_hull_3_points():
+    assert sorted(compute_hull([(1, 2), (3, 4), (5, 5)])) == [(1, 2), (3, 4), (5, 5)]
+    assert sorted(compute_hull([(1, 2), (5, 5), (3, 4)])) == [(1, 2), (3, 4), (5, 5)]
+
+
+def test_split_points():
+    points = [(1, 2), (3, 4), (5, 5), (2, 1), (4, 3)]
+    left, right = split_points(points)
+    assert left == [(1, 2), (2, 1)]
+    assert right == [(3, 4), (4, 3), (5, 5)]
+
+
+def test_keep_between_indecies():
+    points = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
+    assert keep_between_indecies(points, (0, 4)) == [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)]
+    assert keep_between_indecies(points, (4, 0)) == [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0)]
