fc3277c9ca3c3a8043150c88b69343f67b0f24c2
brcktn <29804204+brcktn@users.noreply.github.com>
2025-01-28 10:37:20 -0700

    finished adding time and space complexities in docstrings and comments

--- project2/convex_hull.py
+++ project2/convex_hull.py
@@ -4,27 +4,33 @@
 
 
 def compute_hull(points: list[tuple[float, float]]) -> list[tuple[float, float]]:
-    """Return the subset of provided points that define the convex hull"""
-    if len(points) < 3:
+    """Return the subset of provided points that define the convex hull
+    
+    Time complexity: O(nlogn)
+    Space complexity: O(n)"""
+    if len(points) < 3: # O(1)
         return points
 
-    if len(points) == 3:
+    if len(points) == 3: # O(1)
         if is_ccw(points[0], points[1], points[2]):
             return points
         else:
             return [points[2], points[1], points[0]]
 
-    left, right = split_points(points)
-    left_hull = compute_hull(left)
-    right_hull = compute_hull(right)
-    return combine_hulls(left_hull, right_hull)
+    left, right = split_points(points) # O(nlogn) in first call, O(n) in recursive calls
+    left_hull = compute_hull(left) # O(nlogn) in first call, O(n) in recursive calls
+    right_hull = compute_hull(right) # O(nlogn) in first call, O(n) in recursive calls
+    return combine_hulls(left_hull, right_hull) # O(n)
 
 
 def split_points(
     points: list[tuple[float, float]]
 ) -> tuple[list[tuple[float, float]], list[tuple[float, float]]]:
-    """Return a tuple of two lists of points split along the y-axis"""
-    points.sort(key=lambda x: x[0])
+    """Return a tuple of two lists of points split along the y-axis
+    
+    Time complexity: O(nlogn), O(n) in recursive calls
+    Space complexity: O(n)"""
+    points.sort(key=lambda x: x[0]) # O(nlogn) in first call, O(n) in recursive calls
     mid = len(points) // 2
     return points[:mid], points[mid:]
 
@@ -33,14 +39,17 @@ def combine_hulls(
     left: list[tuple[float, float]], right: list[tuple[float, float]]
 ) -> list[tuple[float, float]]:
     """Return the convex hull of the two provided hulls
-    Assumes that both hulls are sorted in ccw order"""
+    Assumes that both hulls are sorted in ccw order
+    
+    Time complexity: O(n)
+    Space complexity: O(n)"""
     rigthmost_left_index = max(enumerate(left), key=lambda x: x[1][0])[0]   # O(n)
     leftmost_right_index = min(enumerate(right), key=lambda x: x[1][0])[0]  # O(n)
 
     i = rigthmost_left_index
     j = leftmost_right_index
 
-    while True:
+    while True: # worst case O(n)
         if is_ccw(left[(i + 1) % len(left)], left[i], right[j]):
             i = (i + 1) % len(left)
         elif is_ccw(left[i], right[j], right[(j - 1) % len(right)]):
@@ -52,7 +61,7 @@ def combine_hulls(
     i = rigthmost_left_index
     j = leftmost_right_index
 
-    while True:
+    while True: # worst case O(n)
         if is_ccw(right[j], left[i], left[(i - 1) % len(left)]):
             i = (i - 1) % len(left)
         elif is_ccw(right[(j + 1) % len(right)], right[j], left[i]):
@@ -61,14 +70,14 @@ def combine_hulls(
             bottom_chord_indecies = (i, j)
             break
 
-    left = keep_between_indecies(
+    left = keep_between_indecies( # O(n)
         left, (top_chord_indecies[0], bottom_chord_indecies[0])
     )
-    right = keep_between_indecies(
+    right = keep_between_indecies( # O(n)
         right, (bottom_chord_indecies[1], top_chord_indecies[1])
     )
 
-    return left + right
+    return left + right # O(n)
 
 
 def keep_between_indecies(
